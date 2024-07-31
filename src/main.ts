2a32d4940b24d398bf053e85620d359fccbab383
mhhong <hongminghui@baijia.com>
2024-07-31 23:43:10 +0800

    feat: 解决前端路由问题

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import App from './App.vue';
 
 const app = createApp(App);
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory('/fe/'),
 });
 
 app.use(router);
