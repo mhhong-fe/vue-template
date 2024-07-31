8158ce18dcf9e390adc3185be08f1186704ce16d
hongminghui <hongminghui@baijia.com>
2024-07-31 18:27:25 +0800

    feat: 增加dockerfile

--- components.d.ts
+++ components.d.ts
@@ -8,8 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElInput: typeof import('element-plus/es')['ElInput']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     Title: typeof import('./src/components/title/index.vue')['default']
