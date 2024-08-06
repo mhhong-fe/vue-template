55f1b859ffe774a06b7c1f8abb37e4193b805b2e
mhhong <hongminghui@baijia.com>
2024-08-06 17:50:22 +0800

    feat: 测试git merge

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
     <div class="w-100vw h-100vh">
         <h2>Vue Template</h2>
         <RouterView />
-        <el-button @click="jump">应用内跳转</el-button>
+        <el-button @click="jump">
+            应用内跳转
+        </el-button>
     </div>
 </template>
 
